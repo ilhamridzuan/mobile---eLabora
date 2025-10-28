244a96965b32eabe3a12fcab55fcb96a81f57ca4
ilhamridzuan <ilhamridzuaan@gmail.com>
2025-10-28 23:27:15 +0700

    Menambahkan halaman riwayat dan menghubungkan beranda ke fitur lain

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:elabora_app/pages/home_page.dart';
+import 'package:elabora_app/pages/riwayat_page.dart';
 import '../pages/landing_page.dart';
 import '../pages/login_page.dart';
-import '../pages/home_page.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => const LandingPage(),
   '/login': (context) => const LoginPage(),
   '/home': (context) => const HomePage(),
+  '/riwayat': (context) => const RiwayatPage(),
 };
